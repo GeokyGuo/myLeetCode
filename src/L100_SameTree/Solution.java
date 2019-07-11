4b19ac1497e6db83b86457a6f50075b613c820b0
GeokyGuo <jqguo1608@163.com>
2019-07-11 21:50:07 +0800

    新题 83 88 100  有序数组合并，链表删除重复， 二叉树的递归框架

---
+++ src/L100_SameTree/Solution.java
@@ -0,0 +1,41 @@
+package L100_SameTree;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(-1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+        p.left.right = new TreeNode(-1);
+
+        TreeNode q = new TreeNode(-1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+        q.left.right = new TreeNode(0);
+        System.out.println(isSameTree(p, q));
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p != null && q != null && p.val == q.val) {
+
+            return isSameTree(p.right, q.right) & isSameTree(p.left, q.left);
+        } else {
+
+            return false;
+        }
+
+
+    }
+}
