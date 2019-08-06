959e1dbdd19de79015471395eb51637f31dd3423
GeokyGuo <jqguo1608@163.com>
2019-08-06 18:15:21 +0800

    复习 112 求树上的路径和

---
+++ src/L112_PathSum/Solution0806.java
@@ -0,0 +1,42 @@
+package L112_PathSum;
+
+import util.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution0806 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(4);
+//
+//        root.right.left = null;
+//        root.right.right = null;
+//
+//
+//        root.left.left.left = new TreeNode(4);
+//        root.left.left.right = new TreeNode(4);
+
+
+        System.out.println(hasPathSum(root, 6));
+
+    }
+
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null || sum < 0) {
+            return false;
+        }
+        int sub = sum - root.val;
+        if (root.left == null && root.right == null && sub == 0) {
+
+            return true;
+        }
+
+        return hasPathSum(root.left, sub) || hasPathSum(root.right, sub);
+    }
+}
